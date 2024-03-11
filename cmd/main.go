82cc40508ab80d52d11271562775d2f64d7eff7d
Pavel <admaikinpavel@yandex.ru>
2024-03-11 19:08:45 +0300

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	log.Println("Server is running on port 8080")
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
