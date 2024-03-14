ddb422b17d27cec84f3c771571690506b4da923d
Pavel <admaikinpavel@yandex.ru>
2024-03-14 20:44:44 +0300

    feat: add new handler function to server.go

--- internal/models/todoList.go
+++ internal/models/todoList.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"errors"
 	validation "github.com/go-ozzo/ozzo-validation"
 )
 
@@ -20,4 +21,12 @@ func (t *ToDoList) Validate() error {
 type UpdateListInput struct {
 	Title       *string `json:"title"`
 	Description *string `json:"description"`
+}
+
+func (i UpdateListInput) Validate() error {
+	if i.Title == nil && i.Description == nil {
+		return errors.New("update structure has no values")
+	}
+
+	return nil
 }
